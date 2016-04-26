9365fd9323a7749fc15b9350f169219846328f9a
smbuthia <skmbuthia@gmail.com>
2016-04-26 14:28:44 +0300

    tabs to spaces

--- loadImageVector.m
+++ loadImageVector.m
@@ -4,16 +4,16 @@ function[imageVector] = loadImageVector(imageFile, scale)
 imageVector = [];
     % Read the image into a matrix.
     A = imread(imageFile);
-	% Get the dimensions of the matrix - it is a 3D matrix if the image is RGB
-	% and a 2D matrix if the image is grayscale or binary.
+    % Get the dimensions of the matrix - it is a 3D matrix if the image is RGB
+    % and a 2D matrix if the image is grayscale or binary.
     if(size(size(A))(:, 2) == 3) % Check if the image is in RGB
         % If the image is in RGB, it is first converted to grayscale.
-	    A = rgb2gray(A);
+        A = rgb2gray(A);
     endif
-	if(size(size(A))(:, 2) == 2) % Check if the image is in grayscale.
-	    % Resize the image to the given scale.
+    if(size(size(A))(:, 2) == 2) % Check if the image is in grayscale.
+        % Resize the image to the given scale.
         B = imresize(A, scale);
-		% Unroll the image matrix into a vector.
+        % Unroll the image matrix into a vector.
         imageVector = B(:);
-	endif
+    endif
 end
--- main.m
+++ main.m
@@ -11,19 +11,19 @@ y = zeros(mAll,1);
 randVec = randperm(mAll);
 for i = 1:mAll
     fileName = imageFilesList(randVec(i)){1,1};
-	% Replace the backslash character with forward slash to avoid errors due to special characters.
-	fileName(fileName == "\\") = "/"; 
-	% Load the image into a vector
-	X(i,:) = loadImageVector(fileName, 0.01);
-	
-	% Check from the file name whether image is from male or female egg
-	fileName = strsplit(fileName, '/');
-	fname = fileName{1,2};
-	% Female eggs are labelled 1 all others are 0 :)
-	if(regexp(fname, 'F*') == 1)
-	    y(i) == 1;
-	else
-	    y(i) == 0;
-	endif
+    % Replace the backslash character with forward slash to avoid errors due to special characters.
+    fileName(fileName == "\\") = "/"; 
+    % Load the image into a vector
+    X(i,:) = loadImageVector(fileName, 0.01);
+    
+    % Check from the file name whether image is from male or female egg
+    fileName = strsplit(fileName, '/');
+    fname = fileName{1,2};
+    % Female eggs are labelled 1 all others are 0 :)
+    if(regexp(fname, 'F*') == 1)
+        y(i) == 1;
+    else
+        y(i) == 0;
+    endif
 end
 
