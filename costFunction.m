a0bc638041c2696ef18399da04ca53c8af9bc4fc
smbuthia <skmbuthia@gmail.com>
2016-04-26 16:20:25 +0300

    added cost function and regularized cost function.

---
+++ costFunction.m
@@ -0,0 +1,7 @@
+function[J, grad] = costFunction(X, y, Theta1, Theta2)
+% costFunction returns the cost and gradient given the input 
+% matrix X, output vector y, Theta1 and Theta2. This is for a
+% neural network architecture with one hidden layer.
+ 
+
+end
---
+++ costFunctionReg.m
@@ -0,0 +1,8 @@
+function[J, grad] = costFunctionReg(X, y, Theta1, Theta2, lambda)
+% costFunction returns the cost and gradient given the input 
+% matrix X, output vector y, Theta1, Theta2, and regularization 
+% parameter lambda. This is for a neural network architecture 
+% with one hidden layer.
+ 
+
+end
